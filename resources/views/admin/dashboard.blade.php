fca7da2c4af5fe99033d2c5f771674df66496dbe
donaldolog <luis.lopez@rappi.com>
2019-11-05 00:26:01 -0600

    vue

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,12 +9,18 @@
 
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
+    <div id="app">
+        <example-component>
+        </example-component>
+    </div>
 @stop
 
 @section('css')
+    <script src="./css/app.css"></script>
     <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
+    <script src="./js/app.js"></script>
     <script> console.log('Hi!'); </script>
 @stop
