770331ef65c769f01057d1788d90ff3180096f6d
donaldolog <luis.lopez@rappi.com>
2019-11-22 03:31:30 -0600

    auth admin

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -141,7 +141,7 @@
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column {{config('adminlte.classes_sidebar_nav', '')}}" data-widget="treeview" role="menu" @if(config('adminlte.sidebar_nav_animation_speed') != 300) data-animation-speed="{{config('adminlte.sidebar_nav_animation_speed')}}" @endif @if(!config('adminlte.sidebar_nav_accordion')) data-accordion="false" @endif>
                         @auth
-                            @if (\Auth::user()->admin === 1)
+                            @if (\Auth::user()->admin == 1)
                             <li class="nav-item ">
                                 <a class="nav-link " href="{{route('careers.index')}}">
                                     <i class="fas fa-pencil-alt "></i>
